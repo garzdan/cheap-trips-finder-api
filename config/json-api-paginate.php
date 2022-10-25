2f4c5f60facebc8fd53dddbc25835d5d8ef2eef3
daniele <daniele.garziano@risolviamo.com>
2022-10-26 01:24:13 +0200

    fixed bug in flights seeder, configured simple pagination for api endpoints

--- config/json-api-paginate.php
+++ config/json-api-paginate.php
@@ -21,7 +21,7 @@ return [
      * If you only need to display Next and Previous links, you may use
      * simple pagination to perform a more efficient query.
      */
-    'use_simple_pagination' => false, /*
+    'use_simple_pagination' => true, /*
      * Here you can override the base url to be used in the link items.
      */
     'base_url' => NULL, /*
